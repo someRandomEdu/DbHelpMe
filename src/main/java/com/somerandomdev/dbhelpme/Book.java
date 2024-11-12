97d18fc22b3f897f30b78e4665d3283eedb09861
someRandomEdu <23021630@vnu.edu.vn>
2024-11-13 00:40:39 +0700

    13/11/2024

---
+++ src/main/java/com/somerandomdev/dbhelpme/Book.java
@@ -0,0 +1,10 @@
+package com.somerandomdev.dbhelpme;
+
+// TODO
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private String description;
+}
