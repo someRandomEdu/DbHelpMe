dec099e4fdb0ae582f78e978866cef03b78ac8de
someRandomEdu <23021630@vnu.edu.vn>
2024-11-19 21:28:13 +0700

    19/11/2024

--- src/main/java/com/somerandomdev/dbhelpme/views/HomeView.java
+++ src/main/java/com/somerandomdev/dbhelpme/views/HomeView.java
@@ -7,13 +7,9 @@ import com.vaadin.flow.router.Route;
 
 @Route("")
 public class HomeView extends VerticalLayout {
-
     public HomeView() {
-
         add(new H1("Welcome to your new application"));
         add(new Paragraph("This is the home view"));
-
         add(new Paragraph("You can edit this view in ..\\..\\..\\..\\..\\..\\..\\.."));
-
     }
 }
