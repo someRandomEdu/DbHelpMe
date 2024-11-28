b292580277c8ee65948ab06fb82a00a6236482c2
Le Si Thai Son <sonle.25.lsts@gmail.com>
2024-11-29 00:19:37 +0700

    add sign up

---
+++ src/main/java/com/somerandomdev/dbhelpme/AccountSignupCheck.java
@@ -0,0 +1,27 @@
+package com.somerandomdev.dbhelpme;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class AccountSignupCheck {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public boolean accountCheck(String username) {
+        return accountRepository.existsByUsername(username);
+    }
+
+    public Long updateId() {
+        Long maxId = accountRepository.findMaxId();
+        return maxId != null ? maxId + 1 : 1L;
+    }
+
+    public void createAccount(String username, String password) {
+        Account newAccount = new Account(updateId(), username, password, false);
+        System.out.println("Creating new account for: " + username);
+        accountRepository.save(newAccount);
+        System.out.println("Account created successfully for: " + username);
+    }
+}
---
+++ src/main/java/com/somerandomdev/dbhelpme/views/SignupView.java
@@ -0,0 +1,54 @@
+package com.somerandomdev.dbhelpme.views;
+
+import com.somerandomdev.dbhelpme.Account;
+import com.somerandomdev.dbhelpme.AccountSignupCheck;
+import com.somerandomdev.dbhelpme.AppController;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.NativeLabel;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import java.util.Objects;
+
+@Route ("app/signup")
+public class SignupView extends Composite<VerticalLayout> {
+    private TextField newTextField = new TextField("User name");
+    private PasswordField newPasswordField  = new PasswordField("Password");
+    private PasswordField newConfirmPW = new PasswordField("Confirm Password");
+    private Button signUp = new Button("Sign up");
+    private NativeLabel error = new NativeLabel();
+
+    @Autowired
+    private AccountSignupCheck accountSignupCheck = new AccountSignupCheck();
+
+    public SignupView() {
+        signUp.addClickListener(event -> {
+           String username = newTextField.getValue();
+           String password = newPasswordField.getValue();
+           String cfpassword = newConfirmPW.getValue();
+
+           if(!(password.equals(cfpassword))) {
+               error.setText("Password do not match!");
+               return;
+           }
+
+            if (accountSignupCheck.accountCheck(username)) {
+                error.setText("Username already exists!");
+                return;
+            }
+
+           accountSignupCheck.createAccount(username, password);
+            error.setText("Account created successfully!");
+        });
+
+        getContent().add(newTextField, newPasswordField, newConfirmPW, signUp, error);
+        getStyle().set("gap", "var(--lumo-space-m)");
+    }
+}
