03053df91634976416cc836071151bcc7daf4df5
Le Si Thai Son <sonle.25.lsts@gmail.com>
2024-12-02 23:47:45 +0700

    "return and borrow list"

---
+++ src/main/java/com/somerandomdev/dbhelpme/ReturnDataService.java
@@ -0,0 +1,24 @@
+package com.somerandomdev.dbhelpme;
+
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class ReturnDataService {
+
+    private final ReturnDataRepository returnDataRepository;
+
+    public ReturnDataService(ReturnDataRepository returnDataRepository) {
+        this.returnDataRepository = returnDataRepository;
+    }
+
+    // Lưu bản ghi mới
+    public ReturnData save(ReturnData returnData) {
+        return returnDataRepository.save(returnData);
+    }
+
+    // Tìm kiếm bản ghi dựa trên accountId và bookId
+    public Optional<ReturnData> findOneByAccountIdAndBookId(Integer accountId, Integer bookId) {
+        return returnDataRepository.findOneByAccountIdAndBookId(accountId, bookId);
+    }
+}
