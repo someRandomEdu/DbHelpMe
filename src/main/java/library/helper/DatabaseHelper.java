5371207dd9d7a980e3685caf06c272fe9473afb2
MINH TRUONG <vutruongminh6d@gmail.com>
2024-12-03 11:48:11 +0700

    refactor

---
+++ src/main/java/library/helper/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package library.helper;
+
+import java.sql.*;
+
+public class DatabaseHelper {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "minhtruongcn8uetA@";
+    private static Connection connection;
+
+    public static void connectToDatabase() {
+        try {
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connect to database successfully");
+        } catch (SQLException e) {
+            System.err.println("Failed to connect to database " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return connection;
+    }
+}
