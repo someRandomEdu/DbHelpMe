70f7c9b751487bbb2c09e10c29ebd27161bc5a7a
someRandomEdu <23021630@vnu.edu.vn>
2024-12-20 19:28:00 +0700

    20/12/2024 images folder?

--- src/main/java/library/EntryPoint.java
+++ src/main/java/library/EntryPoint.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Theme("my-theme")
-public class EntryPoint implements AppShellConfigurator {
+public class EntryPoint extends SpringBootServletInitializer implements AppShellConfigurator {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EntryPoint.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EntryPoint.class, args);
     }
